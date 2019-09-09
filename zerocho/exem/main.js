935a9c87e31a838e21c54c3dcc12807e82496c9f
goodsungjin <mirepach@naver.com>
2019-09-09 10:03:21 +0900

    feat: push

--- zerocho/exem/main.js
+++ zerocho/exem/main.js
@@ -1,21 +1,20 @@
-function solution(v) {
-  var answer = [
-      []
-  ];
-
-  slice(v);
-
+function solution(words, queries) {
+  var answer = [];
+  
+  for(let i = 0; i < queries.length; i++) {
+    const findQ = queries[i].indexOf('?');
+    let count = 0;
+    
+    for(let j = 0; j < words.length; j++) {
+      if(queries[i].length !== words[j].length) return;
+      if(queries[i].slice(0, findQ) !== words[j].slice(0, findQ)) return;
+      
+      count = count + 1;
+    }
+  }
+  console.log(123)
+  
   return answer;
 }
 
-function slice(v) {
-  let arr = [];
-
-  for(let i = 0; i < v.length; i++) {
-    arr = [...arr, v[i][0], v[i][1]];
-  }
-
-  console.log(arr);
-}
-
-solution([[1, 4], [3, 4], [3, 10]]);
+solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??"]);
